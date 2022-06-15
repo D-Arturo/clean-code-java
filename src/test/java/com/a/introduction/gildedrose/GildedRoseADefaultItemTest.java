3073a9b87b9adac724831a4709b5a29490e4d9c4
arturo.menendez <arturo.menendez@MacBook-Pro-de-arturomenendez.local>
2022-06-15 18:06:44 +0200

    refactored second legacy unit test

--- src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
@@ -9,6 +9,14 @@ public class GildedRoseADefaultItemTest {
 	public static final int NOT_EXPIRED_SELLIN = 15;
 	public static final int DEFAULT_QUALITY = 3;
 	public static final String DEFAULT_ITEM = "DEFAULT_ITEM";
+	public static final int EXPIRED_SELLIN = -1;
+
+	private GildedRose createGildedRoseWithOneItem(String ItemType, int sellin, int quality) {
+		Item item = new Item(ItemType, sellin, quality);
+		Item[] items = new Item[] { item };
+		GildedRose app = new GildedRose(items);
+		return app;
+	}
 
 	@Test
 	public void unexpiredDefaultItem_qualityDecreasesByOneAndSellinByOne() {
@@ -20,13 +28,6 @@ public class GildedRoseADefaultItemTest {
 		assertEquals(DEFAULT_QUALITY - 1, app.items[0].quality);
 	}
 
-	private GildedRose createGildedRoseWithOneItem(String ItemType, int sellin, int quality) {
-		Item item = new Item(ItemType, sellin, quality);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
-		return app;
-	}
-
 	/**
 	 * Method to test the variation in quality of the item for the non expired
 	 * Item.
@@ -35,13 +36,12 @@ public class GildedRoseADefaultItemTest {
 	 *
 	 */
 	@Test
-	public void testUpdateQualityForExpiredItem() {
-		Item item = new Item(DEFAULT_ITEM, -1, 3);
-		Item[] items = new Item[] { item };
-		GildedRose app = new GildedRose(items);
+	public void expiredDefaultItem_qualityDecreasesByTwoAndSellinByOne() {
+		GildedRose app = createGildedRoseWithOneItem(DEFAULT_ITEM, EXPIRED_SELLIN, DEFAULT_QUALITY);
+
 		app.updateQuality();
 		assertEquals(DEFAULT_ITEM, app.items[0].name);
-		assertEquals(-2, app.items[0].sellIn);
-		assertEquals(1, app.items[0].quality);
+		assertEquals(EXPIRED_SELLIN - 1, app.items[0].sellIn);
+		assertEquals(DEFAULT_QUALITY - 2, app.items[0].quality);
 	}
 }
